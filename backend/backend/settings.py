cf42bf92aed251a99fc3f70194404c629560708d
Anton Kravchenko <anton.pzori@yandex.ru>
2026-01-16 01:19:26 +0300

    Fix#1f

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -120,7 +120,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'backend_static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
