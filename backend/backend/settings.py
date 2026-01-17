9f2f1de3b4490dd51dddaae506332a3db6a416ba
Anton Kravchenko <anton.pzori@yandex.ru>
2026-01-18 02:41:57 +0300

    Fix#3d_fix_static!

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -125,8 +125,8 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/backend-static/'
+STATIC_ROOT = '/static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
